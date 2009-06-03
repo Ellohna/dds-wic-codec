7dc77e497a439a91cdba5bb12057c0b3e22ca7fd
ilya.zaytsev <ilya.zaytsev@3568321e-4f0b-11de-a01d-cdf0ff96d5cf>
2009-06-03 00:19:14 +0000

    First commit

---
+++ trunk/dds-wic-codec/dds-wic-codec/dds-wic-codec.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.hpp"
+#include "wicx/regman.hpp"
+#include "wicx/classfactory.hpp"
+#include "ddsx/ddsdecoder.hpp"
+
+STDAPI DllRegisterServer()
+{    
+	wicx::RegMan regMan;
+	ddsx::DDS_Decoder::Register( regMan );
+
+	return S_OK;
+}
+
+STDAPI DllUnregisterServer()
+{    
+	wicx::RegMan regMan;
+	ddsx::DDS_Decoder::Register( regMan );
+	regMan.Unregister();
+
+	return S_OK;
+}
+
+STDAPI DllGetClassObject( REFCLSID rclsid, REFIID riid, void **ppv )
+{    
+	HRESULT result = E_INVALIDARG; 
+
+	if ( NULL != ppv )
+	{
+		IClassFactory *classFactory = NULL;
+		if ( CLSID_DDS_Decoder == rclsid )
+		{
+			result = S_OK;
+			classFactory = new wicx::ClassFactory<ddsx::DDS_Decoder>();
+		}
+		else
+			result = E_NOINTERFACE;
+
+		if ( SUCCEEDED( result ))
+		{
+			if ( NULL != classFactory )
+				result = classFactory->QueryInterface( riid, ppv );
+			else
+				result = E_OUTOFMEMORY;
+		}
+	}
+
+	return result;
+}
+
+BOOL APIENTRY DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved )
+{
+	UNREFERENCED_PARAMETER( lpvReserved );
+
+	switch ( fdwReason )
+	{
+		case DLL_PROCESS_ATTACH:
+			DisableThreadLibraryCalls( hinstDLL );
+			break;
+
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+
+	return TRUE;
+}
